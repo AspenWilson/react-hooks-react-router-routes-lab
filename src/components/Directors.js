eddf6d7978ae4f8a0fc109e7df7f46d6111b17d4
Aspen Wilson <aspen.schell@gmail.com>
2023-04-26 12:02:28 -0700

    completed lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const allDirectors = directors.map((director, index) => {
+    const allMovies = director.movies.map((movie, index) => {
+      return <li key={index}>{movie}</li>
+    })
+    return (
+      <div key={index}>
+        <h3>Name: {director.name}</h3>
+        Movies:
+        <ul>
+          {allMovies}
+          </ul>
+      </div>
+    )
+  })
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {allDirectors}
+  </div>);
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const allMovies = movies.map((movie, index) => {
+    const allGenres = movie.genres.map((genre, index) => {
+      return <li key={index}>{genre}</li>
+    })
+    return (
+      <div key={index}>
+        <h3>Name: {movie.title}</h3>
+        Time:{movie.time}
+        <br />
+        Genres:
+        <ul>
+          {allGenres}
+        </ul>
+      </div>
+    )
+  })
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {allMovies}
+  </div>);
 }
 
 export default Movies;
